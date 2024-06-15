f7017cd6e48322041251f3a7e730761e14974946
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-15 20:55:09 +0530

    Updated background position

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,10 +5,10 @@ import { IoLogoLinkedin } from "react-icons/io5";
 
 function LandingPage() {
   return (
-    <div className="w-screen h-screen bg-[url('/bg.jpeg')] bg-cover flex items-center justify-center">
+    <div className="w-screen h-screen bg-[url('/bg.jpeg')] bg-cover bg-no-repeat bg-center flex items-center justify-center">
       <div class="absolute inset-0 bg-black bg-opacity-90"></div>
-      <div class="relative z-10 p-4">
-        <div className=" text-white poppins-semibold text-[40px] py-10">
+      <div class="relative z-10 p-4 flex flex-col justify-center items-center">
+        <div className=" text-white poppins-semibold text-[40px] py-5">
           Coming Soon<span className="animate-blink">.</span>
           <span className="animate-blink animation-delay-150">.</span>
           <span className="animate-blink animation-delay-300">.</span>
@@ -22,7 +22,7 @@ function LandingPage() {
             <BsTwitterX className="text-white text-[20px]" />
           </a>
           <a href="https://www.linkedin.com/in/praveen-prasad-14764b231/">
-            <IoLogoLinkedin className="text-white text-[20px]" />
+            <IoLogoLinkedin className="text-white text-[25px]" />
           </a>
         </div>
       </div>
